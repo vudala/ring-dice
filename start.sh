e44d67ce59d3210ca787b5f210ead41515d86931
vudalae <vudalae@gmail.com>
2022-08-23 17:31:55 -0300

    fix: corrigindo player id

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 gnome-terminal -- bash -c "./program 0 127.0.0.1 7875 127.0.0.1 7876 1; exec bash"
 gnome-terminal -- bash -c "./program 1 127.0.0.1 7876 127.0.0.1 7877 0; exec bash"
-gnome-terminal -- bash -c "./program 1 127.0.0.1 7877 127.0.0.1 7878 0; exec bash"
+gnome-terminal -- bash -c "./program 2 127.0.0.1 7877 127.0.0.1 7878 0; exec bash"
 gnome-terminal -- bash -c "./program 3 127.0.0.1 7878 127.0.0.1 7875 0; exec bash"
